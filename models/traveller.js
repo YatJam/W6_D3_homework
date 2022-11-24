1f1cd70121cda06fe939b64936cb37569062f154
james yates <yates_7@hotmail.com>
2022-11-24 01:48:33 +0000

    homework donedy done

---
+++ models/traveller.js
@@ -0,0 +1,44 @@
+const Traveller = function(journeys) {
+  this.journeys = journeys;
+};
+
+Traveller.prototype.getJourneyStartLocations = function() {
+    return this.journeys.map((journey) => {
+      return journey.startLocation;
+    });
+};
+
+
+Traveller.prototype.getJourneyEndLocations = function () {
+    return this.journeys.map((journey) => {
+      return journey.endLocation;
+    });
+
+};
+
+Traveller.prototype.getJourneysByTransport = function (transport) {
+    return this.journeys.filter((journey) => {
+      return journey.transport === transport;
+  });
+};
+
+Traveller.prototype.getJourneysByMinDistance = function (minDistance) {
+    return this.journeys.filter((journey) => {
+      return journey.distance >= minDistance;
+  });
+};
+
+Traveller.prototype.calculateTotalDistanceTravelled = function () {
+  return this.journeys.reduce((total, journey) => {
+    return journey.distance + total;
+}, 0);
+};
+
+Traveller.prototype.getUniqueModesOfTransport = function () {
+    return this.journeys.map((journey) => journey.transport) 
+      .filter((value, index, self) => self.indexOf(value) === index)
+
+};
+
+
+module.exports = Traveller;
